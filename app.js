eda1913477a4f840e2636f52607d42b87911327c
natalie sinner <nataliesinner@hotmail.com>
2019-12-14 15:42:10 -0800

    added greeting

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert('hello workd');
+var likeFood = prompt ('Do you like food?');
+console.log(likeFood);
+
+var greeting;
+
+if (likeFood === 'Yes') {
+greeting = "Me too!";
+} else if (likeFood === 'No') {
+    greeting = "How are you still alive?";
+}
+
+console.log('Our responce', greeting);
