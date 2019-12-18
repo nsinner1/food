e3ffb3817588a807e9da36abb576641a5a071e73
natalie sinner <nataliesinner@hotmail.com>
2019-12-17 21:12:45 -0800

    added loop hopefully

--- app.js
+++ app.js
@@ -11,10 +11,12 @@ greeting = "Me too!";
 
 console.log('Our responce', greeting);
 
-for (var i = 0; i < 20; i++){
-    console.log ('How many do you want?');
-    if (i === 20){
-        console.log (i);
-        break;
-    }
+var howMany = function(){
+var count = prompt ('how many things do you want to order?')
 }
+while(count === "" || isNaN(count)){
+    count = prompt('please enter number');
+}
+
+return number(count);
+howMany();
